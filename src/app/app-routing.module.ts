def212d8a3ebf5898b1f10f88325a35970779f30
Saad Mehmood <cracklingflamez40k@gmail.com>
2020-06-29 01:48:40 +0500

    Added a looot of stuff.
    Item page and search pages are functionl (or as much functional as they can be without a backend).
    
    Added a cpanel for items. This allows the administrator of the website to add new items as well as viewing the present items.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component'
+import { SearchPageComponent } from './search-page/search-page.component'
+import { ItemPageComponent } from './item-page/item-page.component' 
+import { ItemCpanelComponent } from './item-cpanel/item-cpanel.component';
 
 const routes: Routes = [
   {
     path : "",
     component: HomePageComponent
+  },
+
+  {
+    path: "search",
+    component: SearchPageComponent
+  },
+
+  {
+    path: "item",
+    component: ItemPageComponent
+  },
+
+  {
+    path: "cpanel/item",
+    component: ItemCpanelComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { ItemPageComponent } from './item-page/item-page.component'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ItemFormComponent } from './item-form/item-form.component'
+import { FormsModule } from '@angular/forms';
+import { ItemCpanelComponent } from './item-cpanel/item-cpanel.component'
 
 @NgModule({
   declarations: [
@@ -16,13 +24,21 @@ import { SearchPageComponent } from './search-page/search-page.component';
     HeaderComponent,
     FooterComponent,
     HomePageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    SearchBarComponent,
+    ItemPageComponent,
+    ItemFormComponent,
+    ItemCpanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxPaginationModule,
+    NgbPaginationModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
